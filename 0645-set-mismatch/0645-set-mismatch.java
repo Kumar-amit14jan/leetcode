2449f1933bc0b52d6340e59abfb8d4a0ccb3df8c
Amit Kumar <51708840+developeramit14jan@users.noreply.github.com>
2022-10-23 14:25:58 +0530

    Time: 64 ms (5.05%), Space: 63.4 MB (5.11%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,11 +1,7 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int arr[] = new int[2];
-        Arrays.sort(nums);
-        int original[] = new int[nums.length];
-        for(int i = 0 ; i< original.length ; i++){
-            original[i] = i+1;
-        }
+        
         HashMap<Integer , Integer> map = new HashMap<Integer , Integer>();
         for(int i = 0 ; i< nums.length ; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
